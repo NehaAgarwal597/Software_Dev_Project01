a7e2782e76ac0002a06abf5e3d8e3a196b175a27
NehaAgarwal597 <nehaagarwala2004@gmail.com>
2026-08-12 23:59:01 +0600

    Update project files

---
+++ package.cpp
@@ -0,0 +1,11 @@
+#include "Package.h"
+
+void Package::display(){
+    cout << "TRIP ID: " << id << endl;
+        cout << "Type: " << type << endl;
+        cout << "Country: " << country << endl;
+        cout << "Destination: " << destination << endl;
+        cout << "Price: " << price << endl;
+        cout << "Duration: " << duration<< endl;
+        cout << "Trip Type: " << tripType << endl;
+}
